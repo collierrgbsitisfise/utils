2f6b02d2c6a9f5da083cfece64db32f267d6c13b
collierrgbsitisfise <vadim.vicolaev.v@gmail.com>
2018-11-13 23:21:42 +0000

    util tool for switching between git local configs

---
+++ git/set-user.js
@@ -0,0 +1,31 @@
+const {
+  exec
+} = require('child_process');
+const accountsData = require('./git.users.json');
+const args = process.argv.slice(2);
+
+const account = args.shift();
+
+const {
+  username,
+  email
+} = accountsData[account] || accountsData['home'];
+
+exec(`git config --global user.name "${username}"`, (error, stdout, stderr) => {
+  if (error) {
+    console.error(`exec set username error: ${error}`);
+    return;
+  }
+
+  console.log('set username: done');
+});
+
+
+exec(`git config --global user.email "${email}"`, (error, stdout, stderr) => {
+  if (error) {
+    console.error(`exec set email error: ${error}`);
+    return;
+  }
+
+  console.log('set email: done');
+});
