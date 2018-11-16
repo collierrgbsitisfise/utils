e798623007798789618b19397e1e427a49a43b3f
collierrgbsitisfise <vadim.vicolaev.v@gmail.com>
2018-11-16 23:07:53 +0000

    delete per key data

--- git/index.js
+++ git/index.js
@@ -1,29 +1,26 @@
 #! /usr/bin/env node
 
-const fs = require('fs');
-const {
-  exec
-} = require('child_process');
-const accountsData = require('./git.users.json');
+const fs = require("fs");
+const { exec } = require("child_process");
+const accountsData = require("./git.users.json");
 const args = process.argv.slice(2);
 
 const account = args.shift();
 
-const {
-  username,
-  email
-} = accountsData[account] || accountsData['home'];
+const { username, email } = accountsData[account] || accountsData["home"];
 
 const set = () => {
-  exec(`git config --global user.name "${username}"`, (error, stdout, stderr) => {
-    if (error) {
-      console.error(`exec set username error: ${error}`);
-      return;
+  exec(
+    `git config --global user.name "${username}"`,
+    (error, stdout, stderr) => {
+      if (error) {
+        console.error(`exec set username error: ${error}`);
+        return;
+      }
+
+      console.log("set username: done");
     }
-
-    console.log('set username: done');
-  });
-
+  );
 
   exec(`git config --global user.email "${email}"`, (error, stdout, stderr) => {
     if (error) {
@@ -31,9 +28,9 @@ const set = () => {
       return;
     }
 
-    console.log('set email: done');
+    console.log("set email: done");
   });
-}
+};
 
 const add = (username, email, key) => {
   const data = {
@@ -42,11 +39,16 @@ const add = (username, email, key) => {
       username,
       email
     }
-  }
+  };
 
-  fs.writeFileSync('git.user.json', JOSN.stringfy(data));
-}
+  fs.writeFileSync("git.user.json", JOSN.stringfy(data));
+};
 
-const getAll = (key) => {
-  fs.writeFileSync()
-}
+const dellete = key => {
+  delete data["key"];
+  fs.writeFileSync("git.user.json", JOSN.stringfy({ ...data }));
+};
+
+const getAll = key => {
+  fs.writeFileSync();
+};
