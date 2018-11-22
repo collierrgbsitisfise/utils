5541f2fba1e2c7e2c9f0e36608d7fd54950bf4b8
vadim_nicolaev_v <vadim.nicolaev@vitamojo.com>
2018-11-22 22:02:11 +0200

    add(define) \commands

---
+++ git/comands.js
@@ -0,0 +1,14 @@
+exports.commands = {
+  add: () => {
+    console.log('add');
+  },
+  set: () => {
+    console.log('set');
+  },
+  delete: () => {
+    console.log('delete');
+  },
+  showAll: () => {
+    console.log('show all');
+  }
+}
--- git/index.js
+++ git/index.js
@@ -1,54 +1,66 @@
 #! /usr/bin/env node
 
 const fs = require("fs");
-const { exec } = require("child_process");
+const {
+  commands
+} = require('./comands.js');
+
+const {
+  exec
+} = require("child_process");
 const accountsData = require("./git.users.json");
 const args = process.argv.slice(2);
 
-const account = args.shift();
+commands[args.shift()]();
+// const args = args.shift();
 
-const { username, email } = accountsData[account] || accountsData["home"];
+// const {
+//   username,
+//   email
+// } = accountsData[account] || accountsData["home"];
 
-const set = () => {
-  exec(
-    `git config --global user.name "${username}"`,
-    (error, stdout, stderr) => {
-      if (error) {
-        console.error(`exec set username error: ${error}`);
-        return;
-      }
+// const set = () => {
+//   exec(
+//     `git config --global user.name "${username}"`,
+//     (error, stdout, stderr) => {
+//       if (error) {
+//         console.error(`exec set username error: ${error}`);
+//         return;
+//       }
 
-      console.log("set username: done");
-    }
-  );
+//       console.log("set username: done");
+//     }
+//   );
 
-  exec(`git config --global user.email "${email}"`, (error, stdout, stderr) => {
-    if (error) {
-      console.error(`exec set email error: ${error}`);
-      return;
-    }
+//   exec(`git config --global user.email "${email}"`, (error, stdout, stderr) => {
+//     if (error) {
+//       console.error(`exec set email error: ${error}`);
+//       return;
+//     }
 
-    console.log("set email: done");
-  });
-};
+//     console.log("set email: done");
+//   });
+// };
 
-const add = (username, email, key) => {
-  const data = {
-    ...accountsData,
-    key: {
-      username,
-      email
-    }
-  };
+// set();
+// const add = (username, email, key) => {
+//   const data = {
+//     ...accountsData,
+//     key: {
+//       username,
+//       email
+//     }
+//   };
 
-  fs.writeFileSync("git.user.json", JOSN.stringfy(data));
-};
+//   fs.writeFileSync("git.user.json", JOSN.stringfy(data));
+// };
 
-const dellete = key => {
-  delete data["key"];
-  fs.writeFileSync("git.user.json", JOSN.stringfy({ ...data }));
-};
+// const dellete = key => {
+//   delete data["key"];
+//   fs.writeFileSync("git.user.json", JOSN.stringfy({ ...data
+//   }));
+// };
 
-const getAll = key => {
-  fs.writeFileSync();
-};
+// const getAll = key => {
+//   fs.writeFileSync();
+// };
