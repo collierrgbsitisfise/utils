4c9255892ee0a9528ddb0da554dac7d9cc7887a9
collierrgbsitisfise <vadim.vicolaev.v@gmail.com>
2018-11-25 21:29:27 +0200

    format output for showAll command

--- git/utils/showAll.js
+++ git/utils/showAll.js
@@ -2,6 +2,13 @@ const fs = require('fs');
 const path = require('path');
 
 exports.showAll = function () {
-  const data = fs.readFileSync(path.resolve('git.users.json'));
-  return String(data);
+  const data = JSON.parse(String(fs.readFileSync(path.resolve('git.users.json'))));
+  Object.values(data).forEach(({
+    username,
+    email
+  }) => {
+    console.log('username: ', username);
+    console.log('email: ', email);
+    console.log('\n');
+  })
 }
