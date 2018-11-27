4031a6170147f456aef3649d3add551bf011b578
collierrgbsitisfise <vadim.vicolaev.v@gmail.com>
2018-11-27 21:38:11 +0200

    comit as right user

--- git/utils/delete.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-const path = require('path');
-
-exports.deleteByKey = function (key) {
-  const data = JSON.parse(String(fs.readFileSync(path.resolve('git.users.json'))));
-  delete data[key];
-  fs.writeFileSync(path.resolve('git.users.json'), JSON.stringify({ ...data
-  }));
-}
