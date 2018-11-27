2ac57f2fc7ffe1180640350d5d8140810c81d2d6
vadim_nicolaev_v <vadim.nicolaev@vitamojo.com>
2018-11-27 21:36:18 +0200

    finish with  basic delete functionality

--- git/utils/delete.js
+++ git/utils/delete.js
@@ -1,4 +1,9 @@
+const fs = require('fs');
+const path = require('path');
+
 exports.deleteByKey = function (key) {
-  console.log('delete key');
-  console.log(key);
+  const data = JSON.parse(String(fs.readFileSync(path.resolve('git.users.json'))));
+  delete data[key];
+  fs.writeFileSync(path.resolve('git.users.json'), JSON.stringify({ ...data
+  }));
 }
